83296455ce12926afe84201924243801bd8c48f0
marciliojr <marcilio3@gmail.com>
2022-09-05 23:29:57 -0300

    Initial commit

---
+++ src/main/java/com/marciliojr/toliso/TolisoApplication.java
@@ -0,0 +1,13 @@
+package com.marciliojr.toliso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolisoApplication.class, args);
+	}
+
+}
